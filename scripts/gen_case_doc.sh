ea1fe03bef16dee41b64eb6e626671c6c493c289
Osier Yang <jyang@redhat.com>
2011-05-06 17:13:19 +0800

    Update script for generating case doc

--- scripts/gen_case_doc.sh
+++ scripts/gen_case_doc.sh
@@ -1,13 +1,11 @@
 #! /bin/sh
-# Summary: create documentation of cases in Python
-# Author: Osier Yang <jyang@redhat.com>
-# Version: 0.1
+# Create documentation of cases in Python
 
 FILES=`find $PWD/repos/Python -type f -name "*.py"`
 
 for i in ${FILES}; do
-    if [ `basename $i` != '__init__.py' ]; then
-        DIRNAME=`dirname $i`
+    if [ $(basename $i) != '__init__.py' ]; then
+        DIRNAME=$(dirname $i)
         DIRNAME=$(echo $DIRNAME | sed -e 's:repos:doc:g')
     fi
 
