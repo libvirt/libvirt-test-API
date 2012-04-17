845c80d621b8edd75b2a67a403690f86e314701f
Osier Yang <jyang@redhat.com>
2012-04-17 18:59:23 +0800

    Remove the unseemly copyright statements in scripts
    There will be a follow up patch to add the copyright statements
    in a better manner.
    
    % for i in $(find . -type f -name "*.py"); do \
        sed -i -e '/copyright/,/www\.gnu\.org/d' $i; \
      done

--- utils/XMLParser.py
+++ utils/XMLParser.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python
 #
-# libvirt-test-API is copyright 2010 Red Hat, Inc.
-#
-# libvirt-test-API is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version. This program is distributed in
-# the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranties of TITLE, NON-INFRINGEMENT,
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-# The GPL text is available in the file COPYING that accompanies this
-# distribution and at <http://www.gnu.org/licenses>.
 #
 # Filename: XMLParser.py
 # Summary: parse and xml document into a python dictionary
