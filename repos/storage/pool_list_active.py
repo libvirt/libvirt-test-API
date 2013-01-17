f275b1a7224d8e33cbce4cee29f8cf6ea9c6143c
Wayne Sun <gsun@redhat.com>
2013-01-17 16:50:09 +0800

    Add storage cases of pool list, pool lookup and volume lookup
    Add 3 simple storage realted API cases
    * pool_list_active is only for testing with flags:
      libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_ACTIVE
    * pool_lookup is for testing lookup pool object by name, UUID,
      UUIDString and volume object
    * vol_lookup is for testing lookup volume object by name, key
      and path
    * add cases into storage_dir.conf
    
    Signed-off-by: Wayne Sun <gsun@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

---
+++ repos/storage/pool_list_active.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# list active storage pool testing with flag:
+# libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_ACTIVE
+
+import libvirt
+from libvirt import libvirtError
+
+from src import sharedmod
+
+required_params = ()
+optional_params = {
+                  }
+
+def pool_list_active(params):
+    """list active storage pool testing"""
+
+    global logger
+    logger = params['logger']
+    namelist = []
+
+    conn = sharedmod.libvirtobj['conn']
+    try:
+        pool_num = conn.numOfStoragePools()
+        logger.info("number of active storage pools is %s" % pool_num)
+
+        flag = libvirt.VIR_CONNECT_LIST_STORAGE_POOLS_ACTIVE
+        poolobj_list = conn.listAllStoragePools(flag)
+        if not len(poolobj_list) == pool_num:
+            logger.error("active pool object number mismatched")
+            return 1
+
+        for i in poolobj_list:
+            pool_name = i.name()
+            namelist.append(pool_name)
+
+        logger.info("active pool name list is %s" % namelist)
+
+        active_namelist = conn.listStoragePools()
+        if namelist == active_namelist:
+            logger.info("active pool name list matched")
+        else:
+            logger.error("active pool name list mismatched")
+            return 1
+
+    except libvirtError, e:
+        logger.error("libvirt call failed: " + str(e))
+        return 1
+
+    logger.info("list active storage pool succeed")
+    return 0
---
+++ repos/storage/vol_lookup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# storage pool volume lookup testing
+
+import libvirt
+from libvirt import libvirtError
+
+from src import sharedmod
+
+required_params = ('poolname', 'volname',)
+optional_params = {
+                  }
+
+def vol_lookup(params):
+    """storage pool volume lookup testing"""
+
+    global logger
+    logger = params['logger']
+    poolname = params['poolname']
+    volname = params['volname']
+    retval = 0
+
+    logger.info("the poolname is %s" % poolname)
+    logger.info("the given volume name is %s" % volname)
+
+    conn = sharedmod.libvirtobj['conn']
+    try:
+        poolobj = conn.storagePoolLookupByName(poolname)
+
+        logger.info("lookup the volume object by name: %s" % volname)
+        volobj = poolobj.storageVolLookupByName(volname)
+        if volobj.name() == volname:
+            logger.info("volume object lookup by name succeed")
+        else:
+            logger.error("volume object lookup by name failed")
+            retval += 1
+
+        volkey = volobj.key()
+        logger.info("lookup the volume object by key: %s" % volkey)
+        volobj_key = conn.storageVolLookupByKey(volkey)
+        if volobj_key.name() == volname:
+            logger.info("volume object lookup by key succeed")
+        else:
+            logger.error("volume object lookup by key failed")
+            retval += 1
+
+        volpath = volobj.path()
+        logger.info("lookup the volume object by path: %s" % volpath)
+        volobj_path = conn.storageVolLookupByPath(volpath)
+        if volobj_path.name() == volname:
+            logger.info("volume object lookup by path succeed")
+        else:
+            logger.error("volume object lookup by path failed")
+            retval += 1
+
+    except libvirtError, e:
+        logger.error("libvirt call failed: " + str(e))
+        return 1
+
+    if retval:
+        return 1
+
+    return 0
