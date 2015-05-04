1a10ac3e2fe729f4db8d62f69b72c9382282bcd4
Luyao Huang <lhuang@redhat.com>
2015-05-04 16:09:04 +0800

    Add a new test case for getMemoryParameters
    Signed-off-by: Luyao Huang <lhuang@redhat.com>

---
+++ repos/virconn/connection_getMemoryParameters.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import libvirt
+from libvirt import libvirtError
+from  utils import utils
+
+required_params = ()
+optional_params = {'conn': ''}
+node_memory = ['full_scans',
+               'merge_across_nodes',
+               'pages_shared',
+               'pages_sharing',
+               'pages_to_scan',
+               'pages_unshared',
+               'pages_volatile',
+               'sleep_millisecs']
+
+SYSFS_MEMORY_SHARED_PATH = '/sys/kernel/mm/ksm/'
+LIBVIRT_API_PATH = '/usr/share/libvirt/api/libvirt-api.xml'
+flags=0
+
+def check_memory_parameter(libvirt_dict, parameter_name):
+    a = libvirt_dict.get('shm_%s' % parameter_name)
+    try:
+        b = long(open('%s%s' % (SYSFS_MEMORY_SHARED_PATH, parameter_name)).read())
+    except IOError:
+        logger.info("Cannot get file in path %s%s" \
+                    % (SYSFS_MEMORY_SHARED_PATH, parameter_name))
+        return 1
+    logger.info("equal %s : libvirt get %u and we get %u"\
+                % (parameter_name, a, b))
+    if a == b:
+        return 0
+    else:
+        return 1
+
+def connection_getMemoryParameters(params):
+    """
+       test API for getMemoryParameters in class virConnect
+    """
+    global logger
+    logger = params['logger']
+    fail=0
+
+    try:
+        conn=libvirt.open(params['conn'])
+
+        logger.info("get connection to libvirtd")
+
+        if utils.check_flags('getMemoryParameters', LIBVIRT_API_PATH) == 1:
+            return 1
+
+        param_dict=conn.getMemoryParameters()
+
+        for n in node_memory:
+            fail=check_memory_parameter(param_dict, n)
+
+    except libvirtError, e:
+        logger.error("API error message: %s" % e.message)
+        fail=1
+    return fail
