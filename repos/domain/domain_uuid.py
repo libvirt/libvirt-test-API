9535d1e664d34d168c72936cda128f9d713561a2
Kairui Song <kasong@redhat.com>
2016-02-01 14:43:49 +0800

    Add test for lookupByUUID
    Change-Id: I9c55babee8bf10bf6c824bb5806dbfc3d067816f
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- repos/domain/domain_uuid.py
+++ repos/domain/domain_uuid.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
-# To test "virsh domuuid" command
-
-import os
-import sys
-import re
+import uuid
 import commands
-
-import libvirt
+from src import sharedmod
 from libvirt import libvirtError
 
 from src import sharedmod
 
-required_params = ()
+required_params = ('guestname',)
 optional_params = {}
 
 VIRSH_DOMUUID = "virsh domuuid"
+RUNNING_DIR = '/var/run/libvirt/qemu'
+CONFIG_DIR = '/etc/libvirt/qemu'
+NEGATIVE_UUID = ['----', None, 123, ]
 
 
 def check_domain_exists(conn, guestname, logger):
-    """ check if the domain exists, may or may not be active """
+    " check if the domain exists, may or may not be active "
     guest_names = []
     ids = conn.listDomainsID()
     for id in ids:
@@ -26,16 +24,14 @@ def check_domain_exists(conn, guestname, logger):
         guest_names.append(obj.name())
 
     guest_names += conn.listDefinedDomains()
-
     if guestname not in guest_names:
         logger.error("%s doesn't exist" % guestname)
         return False
-    else:
-        return True
+    return True
 
 
-def check_domain_uuid(guestname, UUIDString, logger):
-    """ check UUID String of guest """
+def check_domain_uuid_with_virsh(guestname, UUIDString, logger):
+    """ check UUID String of guest with virsh """
     status, ret = commands.getstatusoutput(VIRSH_DOMUUID + ' %s' % guestname)
     if status:
         logger.error("executing " + "\"" + VIRSH_DOMUUID + ' %s' % guestname + "\"" + " failed")
@@ -51,36 +47,83 @@ def check_domain_uuid(guestname, UUIDString, logger):
             return False
 
 
+def check_lookupByUUIDString(conn, domain, UUIDString, logger):
+    if (domain.XMLDesc() !=
+        conn.lookupByUUIDString(UUIDString).XMLDesc()):
+        logger.error("lookupByUUIDString Failed")
+        return False
+    logger.info("lookupByUUIDString success")
+    return True
+
+
+def check_lookupByUUID(conn, domain, UUID, logger):
+    if (domain.XMLDesc() !=
+        conn.lookupByUUID(UUID).XMLDesc()):
+        logger.error("lookupByUUID Failed")
+        return False
+    logger.info("lookupByUUID success")
+    return True
+
+
+def negative_tests(conn, logger):
+    logger.info("Negative test...")
+    for dom_uuid in NEGATIVE_UUID:
+        logger.info("Try UUID %s ..." % str(dom_uuid))
+        success = False
+        try:
+            lookup_result = conn.lookupByUUIDString(dom_uuid)
+        except Exception as e:
+            try:
+                lookup_result = conn.lookupByUUID(dom_uuid)
+            except Exception as e:
+                success = True
+                logger.info("Negative test success with %s" % str(e))
+                return True
+
+        if success is False:
+            logger.error("Negative test failed with uuid %s" % dom_uuid)
+            return False
+
+
 def domain_uuid(params):
-    """check virsh domuuid command
+    """ check domain UUID related APIs
     """
     logger = params['logger']
-    guestname = params['guestname']
-    logger.info("guest name is %s" % guestname)
-
+    guestname = params.get('guestname', None)
     conn = sharedmod.libvirtobj['conn']
-
-    if not check_domain_exists(conn, guestname, logger):
-        logger.error("need a defined guest")
-        return 1
-
-    domobj = conn.lookupByName(guestname)
+    logger.info("guest name is %s" % guestname)
+    domain = conn.lookupByName(guestname)
 
     try:
-        logger.info("get the UUID string of %s" % guestname)
-        UUIDString = domobj.UUIDString()
-        if check_domain_uuid(guestname, UUIDString, logger):
-            logger.info(
-                "UUIDString from API is the same as the one from virsh")
-            logger.info("UUID String is %s" % UUIDString)
-            return 0
-        else:
-            logger.error(
-                "UUIDString from API is not the same as the one from virsh")
+        if not check_domain_exists(conn, guestname, logger):
             return 1
+
+        logger.info("get the UUID string of %s" % domain.name())
+        UUIDString = domain.UUIDString()
+        logger.info("UUID String is %s" % UUIDString)
+
+        if uuid.UUID(UUIDString).bytes != domain.UUID():
+            logger.info("Raw UUID don't match UUID String")
+            return 1
+        logger.info("Raw UUID match UUID String")
+
+        if not check_lookupByUUIDString(conn, domain, UUIDString, logger):
+            return 1
+
+        if not check_lookupByUUID(conn, domain, uuid.UUID(UUIDString).bytes, logger):
+            return 1
+
     except libvirtError, e:
         logger.error("API error message: %s, error code is %s"
                      % (e.message, e.get_error_code()))
+        return 1
+
+    if not check_domain_uuid_with_virsh(domain.name(), UUIDString, logger):
+        logger.error("UUIDString from API is not the same as the one from virsh")
+        return 1
+    logger.info("UUIDString from API is the same as the one from virsh")
+
+    if not negative_tests(conn, logger):
         return 1
 
     return 0
