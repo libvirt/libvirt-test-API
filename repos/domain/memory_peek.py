5b37cef86278a2b160c698ef8d416b8360113249
Kairui Song <kasong@redhat.com>
2016-03-23 14:31:21 +0800

    Fix memory_peek.py
    When reading memory of vm using virtual memory address, skip unavaliable
    memory address by adding page offset to avoid 'Invalid memory' error.
    
    Change-Id: I55d3e29058ec761f7d4bcb0c65a7d35889e59c71
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- repos/domain/memory_peek.py
+++ repos/domain/memory_peek.py
@@ -7,7 +7,7 @@ from libvirt import libvirtError
 from src import sharedmod
 
 required_params = ('guestname', )
-optional_params = {}
+optional_params = {'page_offset':''}
 
 
 def memory_peek(params):
@@ -15,8 +15,12 @@ def memory_peek(params):
     """
     logger = params['logger']
     guestname = params['guestname']
+    #If page_offset not given, use Linux default page_offset for x86
+    page_offset = int(params.get('page_offset', 0xffff880000000000), 0)
 
-    flag_dict = {'1': "VIR_MEMORY_VIRTUAL", '2': "VIR_MEMORY_PHYSICAL"}
+    flag_dict = {1: "VIR_MEMORY_VIRTUAL", 2: "VIR_MEMORY_PHYSICAL"}
+    addr_dict = {1: page_offset,
+                 2: 0x0000000000000000}
 
     logger.info("the name of virtual machine is %s" % guestname)
 
@@ -27,7 +31,7 @@ def memory_peek(params):
         logger.info("test memory peek API")
         for flag in flag_dict.keys():
             logger.info("using flag: %s" % flag_dict[flag])
-            mem = domobj.memoryPeek(0, 0, int(flag))
+            mem = domobj.memoryPeek(addr_dict[flag], 0, flag)
             if mem:
                 return 1
             logger.info("memory peek API works fine with flag: %s" %
@@ -36,7 +40,7 @@ def memory_peek(params):
         logger.info("peek 8 bytes from domain memory")
         for flag in flag_dict.keys():
             logger.info("using flag: %s" % flag_dict[flag])
-            mem = domobj.memoryPeek(0, 8, int(flag))
+            mem = domobj.memoryPeek(addr_dict[flag], 8, flag)
             if not mem:
                 return 1
             logger.info("8 bytes start with 0 with flag %s is: %s" %
