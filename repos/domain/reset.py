24c618c35d8ad8ad7d970f4523bef4433890db26
CheJian <jiche@redhat.com>
2017-11-20 13:52:37 +0800

    Add new cases for reset
    Change-Id: I6c9e29a941be28a107c64297184753ea4b657f63
    Signed-off-by: CheJian <jiche@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/123636
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Reviewed-by: Kairui Song <kasong@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

---
+++ repos/domain/reset.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from libvirt import libvirtError
+from src import sharedmod
+from utils import utils
+
+required_params = ('guestname', 'username', 'password')
+optional_params = {}
+
+
+def get_num_row(domobj, logger, username, password, ip):
+    #get the current row number of "last reboot"'result
+    cmd = "last reboot | wc -l"
+    ret, output = utils.remote_exec_pexpect(ip, username, password, cmd)
+    if ret:
+        logger.error("fail to remote exec cmd")
+        return None
+    logger.debug("the cmd output is %s" % output)
+    return int(output)
+
+
+def reset(params):
+    """Reset a domain immediately without any guest OS shutdown
+       Return 0 on SUCCESS or 1 on FAILURE
+       Note:reset function just a reset of hardware,it don't shutdown guest.
+            Resetting a virtual machine does not apply any pending domain configuration
+            changes. Changes to the domain's configuration only take effect after a
+            complete shutdown and restart of the domain.
+    """
+    guestname = params['guestname']
+    logger = params['logger']
+
+    username = params['username']
+    password = params['password']
+
+    conn = sharedmod.libvirtobj['conn']
+    domobj = conn.lookupByName(guestname)
+
+    logger.info("get the mac address of vm %s" % guestname)
+    mac = utils.get_dom_mac_addr(guestname)
+    logger.info("the mac address of vm %s is %s" % (guestname, mac))
+    logger.info("get ip by mac address")
+    ip = utils.mac_to_ip(mac, 180)
+    logger.info("the ip address of vm %s is %s" % (guestname, ip))
+    logger.info("reset vm %s now" % guestname)
+
+    old_times = get_num_row(domobj, logger, username, password, ip)
+    if old_times is None:
+        return 1
+
+    #reset domain
+    try:
+        domobj.reset(0)
+    except libvirtError, e:
+        logger.error("API error message: %s, error code is %s"
+                     % (e.message, e.get_error_code()))
+        logger.error("fail to reset domain")
+        return 1
+
+    new_times = get_num_row(domobj, logger, username, password, ip)
+    if new_times is None:
+        return 1
+    if new_times != old_times + 1:
+        logger.error("fail to reset guest")
+        return 1
+
+    logger.info("vm %s reset successfully" % guestname)
+    logger.info("PASS")
+    return 0
