e1ddded30e67cdd40204759dfaf34b60da121d8d
Liping Cheng <lcheng@redhat.com>
2018-07-31 18:48:00 +0800

    Add case for launchSecurityInfo()
    Change-Id: I6853df16afb62e988a641a6a29fd3eb89f3e73cd
    Signed-off-by: Liping Cheng <lcheng@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/145599
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

---
+++ repos/domain/launch_security_info.py
@@ -0,0 +1,31 @@
+import libvirt
+
+from libvirt import libvirtError
+
+from src import sharedmod
+from utils import utils
+
+required_params = ('guestname',)
+optional_params = {}
+
+
+def launch_security_info(params):
+    guestname = params['guestname']
+    logger = params['logger']
+
+    if not utils.version_compare("libvirt-python", 4, 5, 0, logger):
+        logger.info("Current libvirt-python don't support launchSecurityInfo().")
+        return 0
+
+    conn = sharedmod.libvirtobj['conn']
+    dom = conn.lookupByName(guestname)
+
+    try:
+        info = dom.launchSecurityInfo(0)
+        logger.info("get launch security info: %s" % info)
+    except libvirtError as e:
+        logger.error("API error message: %s, error code is %s"
+                     % (e.get_error_message(), e.get_error_code()))
+        return 1
+    logger.info('PASS')
+    return 0
