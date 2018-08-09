b6c2deecf4b90a8fdc490320efe90b121ac1b955
Liping Cheng <lcheng@redhat.com>
2018-08-09 18:41:07 +0800

    Add test information for migration case
    Change-Id: Iab159a0dff56d6c758a206244057d1631b6de508
    Signed-off-by: Liping Cheng <lcheng@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/146573
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- repos/domain/migrate.py
+++ repos/domain/migrate.py
@@ -244,6 +244,10 @@ def migrate(params):
             return 1
 
     try:
+        ## Add for test
+        logger.info("xml for migration: %s\n\n" % srcdom.XMLDesc(libvirt.VIR_DOMAIN_XML_MIGRATABLE))
+        logger.info("xml: %s\n\n" % srcdom.XMLDesc())
+        ## End for test
         if(migflags & libvirt.VIR_MIGRATE_PEER2PEER):
             if domxml is None:
                 logger.info("use migrateToURI() to migrate")
