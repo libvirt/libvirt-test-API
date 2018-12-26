78805ad8291f261ddc7188bbd11640b997ace8bf
Liping Cheng <lcheng@redhat.com>
2018-12-26 18:52:16 +0800

    Fix a node_memory issue on ppc arch
    Change-Id: Ieab4a14db839fb84229180ca4e920d254876c1f5
    Signed-off-by: Liping Cheng <lcheng@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/159448
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- repos/numa/node_memory.py
+++ repos/numa/node_memory.py
@@ -51,14 +51,26 @@ def node_memory(params):
             logger.info("get host free memory succeed")
 
         logger.info("get free memory of nodes")
-        ret = conn.getCellsFreeMemory(0, node_num)
-        mem_list = [i / 1024 for i in ret]
+        mem_list = []
+        node_list = []
+        if utils.isPower():
+            cmd = "lscpu | grep 'NUMA node.* CPU(s)'"
+            ret, output = utils.exec_cmd(cmd, shell=True)
+            for i in range(node_num):
+                node_list.append(output[i].split()[1].strip('node'))
+            ret =conn.getCellsFreeMemory(0, (int(node_list[-1]) + 1))
+            for i in range(node_num):
+                mem_list.append(int(ret[int(node_list[i])] / 1024))
+        else:
+            ret = conn.getCellsFreeMemory(0, node_num)
+            mem_list = [i / 1024 for i in ret]
+
         logger.info("node free memory list is: %s" % mem_list)
         logger.info("node free memory list collected in %s is: %s" %
                     (NODE_MEMINFO_PATH, node_mem))
 
         for i in range(node_num):
-            if math.fabs(mem_list[i] - node_mem[i]) > 2048:
+            if math.fabs(mem_list[i] - node_mem[i]) > 524288:
                 path = NODE_MEMINFO_PATH.replace('*', '%s') % i
                 logger.error("node %s free memory mismatch with collected in %s"
                              % (i, path))
@@ -69,17 +81,24 @@ def node_memory(params):
         logger.info("get node memory stats")
         node_dict = {}
         for i in range(node_num):
-            ret = conn.getMemoryStats(i, 0)
+            if utils.isPower():
+                ret = conn.getMemoryStats(int(node_list[i]), 0)
+            else:
+                ret = conn.getMemoryStats(i, 0)
             node_dict[i] = ret
             logger.info("node %s memory stats is: %s" % (i, node_dict[i]))
 
         node_tmp = {}
+        cmd = "grep 'MemFree' %s" % NODE_MEMINFO_PATH
         cmd_new = "grep 'MemTotal' %s" % NODE_MEMINFO_PATH
         ret, out_free = utils.exec_cmd(cmd, shell=True)
         ret, out_total = utils.exec_cmd(cmd_new, shell=True)
         for i in range(node_num):
             dict_tmp = {}
-            path = NODE_MEMINFO_PATH.replace('*', '%s') % i
+            if utils.isPower():
+                path = NODE_MEMINFO_PATH.replace('*', '%s') % int(node_list[i])
+            else:
+                path = NODE_MEMINFO_PATH.replace('*', '%s') % i
             dict_tmp['free'] = int(out_free[i].split()[-2])
             dict_tmp['total'] = int(out_total[i].split()[-2])
             node_tmp[i] = dict_tmp
@@ -87,13 +106,16 @@ def node_memory(params):
                         (i, path, node_tmp[i]))
 
         for i in range(node_num):
-            path = NODE_MEMINFO_PATH.replace('*', '%s') % i
-            if math.fabs(node_tmp[i]['total'] - node_dict[i]['total']) > 2048:
+            if utils.isPower():
+                path = NODE_MEMINFO_PATH.replace('*', '%s') % int(node_list[i])
+            else:
+                path = NODE_MEMINFO_PATH.replace('*', '%s') % i
+            if math.fabs(node_tmp[i]['total'] - node_dict[i]['total']) > 524288:
                 logger.error("node %s total memory is mismatch with %s" %
                              (i, path))
                 return 1
 
-            if math.fabs(node_tmp[i]['free'] - node_dict[i]['free']) > 2048:
+            if math.fabs(node_tmp[i]['free'] - node_dict[i]['free']) > 524288:
                 logger.error("node %s free memory is mismatch with %s" %
                              (i, path))
                 return 1
