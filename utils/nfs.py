391dd70fa12c9bfe852de78e1704f5d4da60aa10
Liping Cheng <lcheng@redhat.com>
2019-01-17 16:34:41 +0800

    Fix nfs server problem on RHEL 7
    Change-Id: I261efee5541fa61713358f1be22560cfbde2c684
    Signed-off-by: Liping Cheng <lcheng@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/160868
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- utils/nfs.py
+++ utils/nfs.py
@@ -9,7 +9,10 @@ from . import utils
 
 
 def local_nfs_exported(nfs_path, logger):
-    cmd = "exportfs -o rw,fsid=0,no_root_squash *:%s" % nfs_path
+    if utils.isRelease('7', logger):
+        cmd = "exportfs -o rw,no_root_squash *:%s" % nfs_path
+    else:
+        cmd = "exportfs -o rw,fsid=0,no_root_squash *:%s" % nfs_path
     ret = process.run(cmd, shell=True, ignore_status=True)
     if ret.exit_status:
         logger.error("%s failed: %s." % (cmd, ret.stdout))
