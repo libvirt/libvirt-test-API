4fcb0aea9150e6f2a9642b122ba0a24507673385
Liping Cheng <lcheng@redhat.com>
2019-02-02 15:09:34 +0800

    Fix a issue about find_netfs_pool_sources
    Add a check for glusterfs format type in find_netfs_pool_sources.py.
    
    Change-Id: Ida08f505f62c8b80a248d4ac9a4b1a1bdcc8fc86
    Signed-off-by: Liping Cheng <lcheng@redhat.com>
    Reviewed-on: https://code.engineering.redhat.com/gerrit/162147
    Tested-by: Libvirt Jenkins <libvirt-jenkins@redhat.com>
    Signed-off-by: Liping Cheng <lcheng@redhat.com>

--- repos/storage/find_netfs_pool_sources.py
+++ repos/storage/find_netfs_pool_sources.py
@@ -15,29 +15,54 @@ def check_pool_sources(host, xmlstr):
     """check the netfs sources with command:
        showmount --no-headers -e HOSTNAME
     """
-    source_val = []
-
+    source_val = {}
+    format_type = ""
+    path_nfs = []
+    path_gluster = []
     doc = minidom.parseString(xmlstr)
     for diskTag in doc.getElementsByTagName("source"):
-        device_element = diskTag.getElementsByTagName("dir")[0]
-        attr = device_element.getAttributeNode('path')
-        path_val = attr.nodeValue
+        path_val = diskTag.getElementsByTagName("dir")[0].getAttributeNode('path').nodeValue
+        format_type = diskTag.getElementsByTagName("format")[0].getAttributeNode('type').nodeValue
+        if format_type == "nfs":
+            path_nfs.append(path_val)
+        elif format_type == "glusterfs":
+            path_gluster.append(path_val)
+        else:
+            logger.error("Need to check %s type." % format_type)
+            return 1
 
-        source_val.append(path_val)
+    if len(path_nfs) > 0:
+        source_val['nfs'] = path_nfs
+    if len(path_gluster) > 0:
+        source_val['glusterfs'] = path_gluster
+    logger.debug("pool source info: %s" % source_val)
 
-    logger.debug("pool source info list is: %s" % source_val)
+    nfs_path_list = []
+    gluster_path_list = []
+    for key,value in source_val.items():
+        if key == "nfs":
+            cmd = "showmount --no-headers -e %s | awk -F' ' '{print $1}'" % host
+            ret, nfs_path_list = utils.exec_cmd(cmd, shell=True)
+            logger.debug("showmount command output list is: %s" % nfs_path_list)
+            if nfs_path_list != value:
+                logger.error("nfs pool sources don't match.")
+                return 1
+        elif key == "glusterfs":
+            cmd = "grep '%s' /etc/hosts" % host
+            ret, host_ip = utils.exec_cmd(cmd, shell=True)
+            logger.debug("glusterfs host ip: %s" % host_ip[0].split(' ')[0])
+            cmd = "gluster volume list --remote-host=%s" % host_ip[0].split(' ')[0]
+            ret, gluster_path_list = utils.exec_cmd(cmd, shell=True)
+            logger.debug("gluster type: %s" % gluster_path_list)
+            if gluster_path_list != value:
+                logger.error("glusterfs pool sources don't match.")
+                return 1
+        else:
+            logger.error("Need to check %s type." % format_type)
+            return 1
 
-    cmd = "showmount --no-headers -e %s | awk -F' ' '{print $1}'" % host
-    ret, path_list = utils.exec_cmd(cmd, shell=True)
-
-    logger.debug("showmount command output list is: %s" % path_list)
-
-    if source_val == path_list:
-        logger.info("source list matched with showmount command output")
-        return 0
-    else:
-        logger.error("source list did not match with showmount command output")
-        return 1
+    logger.info("pool sources list match.")
+    return 0
 
 
 def find_netfs_pool_sources(params):
